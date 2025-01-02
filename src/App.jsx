e5743c0fb2eeeaac6de37196fac3007a75f84804
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-01-03 04:07:53 +0530

    functsion added

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,33 @@ import { TodoProvider } from "./contexts/TodoContext";
 
 function App() {
   const { todos, setTodos } = useState([]);
+   
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
   return (
-    <TodoProvider>
+    <TodoProvider value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-5">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
